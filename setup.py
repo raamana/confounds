10c56975949a05a43dd5474154fddecf9ed6ef93
Pradeep Reddy Raamana <raamana@gmail.com>
2019-07-19 11:32:45 -0400

    updating goals in pkg setup

--- setup.py
+++ setup.py
@@ -27,7 +27,28 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    description="conquering confounds and covariates in machine learning",
+    description="""Conquering confounds and covariates in machine learning
+    
+Vision / Goals
+~~~~~~~~~~~~~~~
+
+The high-level goals of this package is to develop high-quality library to conquer confounds and covariates in ML applications. By conquering, we mean methods and tools to
+
+ 1. visualize and establish the presence of confounds (e.g. quantifying confound-to-target relationships),
+ 2. offer solutions to handle them appropriately via correction or removal etc, and
+ 3. analyze the effect of the deconfounding methods in the processed data (e.g. ability to check if they worked at all, or if they introduced new or unwanted biases etc).
+
+
+Methods
+~~~~~~~~
+
+ - Residualize (e.g. via regression)
+ - Augment (include confounds as predictors)
+ - Harmonize (correct batch effects via rescaling or normalization etc)
+ - Stratify (sub- or resampling procedures to minimize confounding)
+ - Utilities (Goals 1 and 3)
+
+""",
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description="conquering confounds and covariates in machine learning",
