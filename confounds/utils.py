815bb9cc20d029bf8363e96b5168e16875c5aa7a
Pradeep Reddy Raamana <raamana@gmail.com>
2019-07-21 12:15:23 -0400

    building wishlist for helpers

---
+++ confounds/utils.py
@@ -0,0 +1,49 @@
+"""
+Utilities and helpers to enable a systematic study of various aspects related to
+confounds
+
+"""
+
+def confound_restricted_permutations(target, confounds):
+    """
+    Variant of permutation testing to restrict relabelling of the target within
+    certain values/ranges of the confounds, to preserve feature-to-confound
+    relationships, but break the feature-to-target relationships. Such
+    permutations would help us test whether models are learning the
+    confound-related or target-related info i.e. if the prediction performance is
+    high even when target is permuted, we can infer model is learning the
+    confound-related info.
+
+    Parameters
+    ----------
+    target
+    confounds
+
+    Returns
+    -------
+
+    """
+
+    raise NotImplementedError()
+
+
+def score_stratified_by_confound(score, confounds):
+    """
+    Helper to summarize the performance score (accuracy, MSE, MAE etc) for each
+    level or variant of confound. This is helpful to assess any bias towards a
+    particular value when confounds are categorical (such as site or gender). So
+    if the MSE (of target) for Females is much lower compared to Males, then it
+    may indicate a potential bias of the model towards Females (due to imbalance in
+    size?)
+
+    Parameters
+    ----------
+    score
+    confounds
+
+    Returns
+    -------
+
+    """
+
+    raise NotImplementedError()
