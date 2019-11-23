5999792dc69cdbbd0814eb5084ec9ae524512acb
Pradeep Reddy Raamana <raamana@gmail.com>
2019-11-23 18:59:49 -0500

    basic test for estimators

--- confounds/tests/test_confounds.py
+++ confounds/tests/test_confounds.py
@@ -3,13 +3,22 @@
 
 """Tests for `confounds` package."""
 
-import pytest
-
-
-from confounds.base import Augment, Residualize, DummyDeconfounding
+from confounds.base import Residualize, DummyDeconfounding
+from sklearn.utils.estimator_checks import check_estimator
 from sklearn.datasets import make_classification
 
 X, y = make_classification()
+
+
+def test_estimator_API():
+
+
+    for est in (Residualize, DummyDeconfounding):
+        try:
+            check_estimator(est)
+            print('{} passes estimator checks'.format(est.__name__))
+        except:
+            raise
 
 
 def test_method_does_not_introduce_bias():
@@ -17,3 +26,4 @@ def test_method_does_not_introduce_bias():
     Test to ensure any deconfounding method does NOT introduce bias in a sample
     when confounds not have any relationship with the target!
     """
+
