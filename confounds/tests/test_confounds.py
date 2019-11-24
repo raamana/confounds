44db6aa356aa1d8620d9dd3ef4df66ae0cee5fa7
Pradeep Reddy Raamana <raamana@gmail.com>
2019-11-23 19:44:16 -0500

    quick test for augment

--- confounds/tests/test_confounds.py
+++ confounds/tests/test_confounds.py
@@ -3,22 +3,35 @@
 
 """Tests for `confounds` package."""
 
-from confounds.base import Residualize, DummyDeconfounding
+from confounds.base import Residualize, DummyDeconfounding, Augment
 from sklearn.utils.estimator_checks import check_estimator
 from sklearn.datasets import make_classification
-
-X, y = make_classification()
-
+import numpy as np
 
 def test_estimator_API():
 
 
-    for est in (Residualize, DummyDeconfounding):
+    for est in (Residualize, Augment, DummyDeconfounding):
         try:
             check_estimator(est)
             print('{} passes estimator checks'.format(est.__name__))
         except:
             raise
+
+def test_augment():
+
+    max_dim = 100
+    for num_confounds in np.random.randint(1, max_dim, 3):
+
+        X_all, y = make_classification(n_features=max_dim+10)
+
+        X = X_all[:, :-num_confounds]
+        confounds = X_all[:, -num_confounds:]
+
+        aug = Augment()
+        aug.fit(X, confounds)
+        X_aug = aug.transform(X, confounds)
+        assert np.all(X_aug==X_all)
 
 
 def test_method_does_not_introduce_bias():
