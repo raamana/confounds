b89b24ced119cc8f59661a82ec1b1d696f9a1a68
Pradeep Reddy Raamana <raamana@gmail.com>
2020-03-09 13:29:33 -0400

    updated tag [skip ci]

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+0.g739ec48.dirty',
+    VERSION: '0.1.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
