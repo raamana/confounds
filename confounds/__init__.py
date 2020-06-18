ae053f1ace273d0a85d25ba28a6b4686eb2480be
zuxfoucault <zuxfoucault@gmail.com>
2020-06-18 22:25:09 +0800

    undo __init__.py

--- confounds/__init__.py
+++ confounds/__init__.py
@@ -18,7 +18,3 @@ else:
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-
-#from .tests import (test_residualize_linear,
-#                    test_residualize_targets_linear)
