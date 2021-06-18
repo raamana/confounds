078e7905c8cad3df2b0381d4587af5ae5ec9ee93
James Chapman <james.chapman.19@ucl.ac.uk>
2021-06-18 17:30:50 +0100

    partial correlation with t test

--- confounds/__init__.py
+++ confounds/__init__.py
@@ -11,6 +11,8 @@ from sys import version_info
 if version_info.major >= 3:
     from confounds.base import Residualize, Augment, DummyDeconfounding, \
         ConfoundsException
+    from confounds.visualize import pairs
+    from confounds.metrics import partial_correlation,prediction_partial_correlation
 else:
     raise NotImplementedError('confounds library requires Python 3 or higher! ')
 
