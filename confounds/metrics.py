f856d9d2d7f3d1ec5c1e4b524edeb7e5a8972743
James Chapman <james.chapman.19@ucl.ac.uk>
2021-06-18 15:38:41 +0100

    partial correlation with t test

--- confounds/metrics.py
+++ confounds/metrics.py
@@ -6,3 +6,39 @@ degree of variability across different confound levels or groups,
 degree of harmonization achieved (e.g. reduction in variance of means/medians)
 
 """
+
+import numpy as np
+
+from confounds import Residualize
+from scipy import stats
+
+
+def partial_correlation(predictions, targets, confounds, significance_test=True):
+    """
+
+    Parameters
+    ----------
+    predictions :
+    targets :
+    confounds :
+    significance_test :
+
+    Returns
+    -------
+
+    """
+    res1=Residualize()
+    res1.fit(predictions, confounds)
+    deconfound_predictions = res1.transform(predictions, confounds)
+    res2 = Residualize()
+    res2.fit(targets, confounds)
+    deconfound_targets = res2.transform(targets, confounds)
+    partial_correlation = np.corrcoef(deconfound_predictions, deconfound_targets, rowvar=False)[0, 1]
+
+    if significance_test:
+        n = deconfound_predictions.shape[0]
+        g = confounds.shape[1]
+        t = partial_correlation * np.sqrt((n - 2 - g) / (1 - partial_correlation ** 2))
+        statistical_significance = stats.t.sf(np.abs(t), df=n - 1)
+        return partial_correlation, statistical_significance
+    return partial_correlation
